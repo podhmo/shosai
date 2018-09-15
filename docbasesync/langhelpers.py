a2dd0f91a1d70a50fedd4c1df3b3016fb432befe
podhmo <ababjam61+github@gmail.com>
2018-09-15 19:51:14 +0900

    feat: namestore

--- docbasesync/langhelpers.py
+++ docbasesync/langhelpers.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class reify(object):
     """cached property"""
 
@@ -14,3 +17,33 @@ class reify(object):
         val = self.wrapped(inst)
         setattr(inst, self.wrapped.__name__, val)
         return val
+
+
+class NameStore:
+    def __init__(self):
+        self.c = defaultdict(int)
+        self.value_map = {}  # (src_type, dst_type) => (name, i)
+
+    def __contains__(self, value):
+        return value in self.value_map
+
+    def __setitem__(self, value, name):
+        if value not in self.value_map:
+            self.value_map[value] = self.get_name(value, name)
+            self.c[name] += 1
+
+    def __getitem__(self, value):
+        return self.value_map[value]
+
+    def get_name(self, value, name):
+        try:
+            return self[value]
+        except KeyError:
+            i = self.c[name]
+            return self.new_name(name, i)
+
+    def new_name(self, name, i):
+        if i == 0:
+            return name
+        else:
+            return "{}{:02d}".format(name, i)
---
+++ docbasesync/tests/test_namestore.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+class NameStoreTests(unittest.TestCase):
+    def _makeOne(self):
+        from docbasesync.langhelpers import NameStore
+        return NameStore()
+
+    def test_access_by_same_key(self):
+        target = self._makeOne()
+        one = object()
+        target[one] = "foo"
+        self.assertEqual(target[one], "foo")
+        target[one] = "foo"
+        self.assertEqual(target[one], "foo")
+
+    def test_access_by_not_same_keys(self):
+        target = self._makeOne()
+        one = object()
+        two = object()
+        three = object()
+
+        target[one] = "foo"
+        self.assertEqual(target[one], "foo")
+
+        target[two] = "foo"
+        self.assertEqual(target[two], target.new_name("foo", 1))
+        self.assertEqual(target[two], "foo01")
+
+        target[three] = "foo"
+        self.assertEqual(target[one], "foo")
+        self.assertEqual(target[two], "foo01")
+        self.assertEqual(target[three], "foo02")
+
+    def test_access_different_positions(self):
+        target = self._makeOne()
+        one = object()
+        two = object()
+        target[one] = "foo"
+        self.assertEqual(target[one], "foo")
+        target[two] = "bar"
+        self.assertEqual(target[two], "bar")
