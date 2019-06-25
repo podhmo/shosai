b29257b00a2b843992d372aa4eda83b25a474856
podhmo <ababjam+github@gmail.com>
2019-06-25 16:45:06 +0900

    bump-version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ testing_extras = tests_require + []
 
 setup(
     name="shosai",
-    version="0.0.3",
+    version="0.0.4",
     description="-",
     long_description=README + "\n\n" + CHANGES,
     classifiers=[
