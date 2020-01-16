b9918a399d38d714672c30cd347c97cfe25fce7c
podhmo <ababjam61+github@gmail.com>
2020-01-17 08:08:20 +0900

    feat: docbase, auth doc url

--- shosai/docbase/configuration.py
+++ shosai/docbase/configuration.py
@@ -7,12 +7,14 @@ from . import structure
 
 logger = logging.getLogger(__name__)
 
-API_TOKEN_SKELETON: str = "<from https://help.docbase.io/posts/45703#%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B9%E3%83%88%E3%83%BC%E3%82%AF%E3%83%B3>"
+API_TOKEN_SKELETON: str = "<token>"
 CONFIG_SKELETON: structure.ProfileDict = {
     "teamname": "<team>",
     "token": API_TOKEN_SKELETON,
     "username": "<user>",
 }
+
+AUTH_DOC_URL = "https://help.docbase.io/posts/45703#%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B9%E3%83%88%E3%83%BC%E3%82%AF%E3%83%B3%E4%BD%9C%E6%88%90%E6%96%B9%E6%B3%95"
 
 
 def init(path: str) -> None:
